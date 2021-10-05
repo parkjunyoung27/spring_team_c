0b12238c9ee0e82f0377a05c4871a170cc06aed3
muDaeri <84271540+muDaeri@users.noreply.github.com>
2021-10-05 17:53:03 +0900

    board update

--- GSupport/src/main/java/com/team_c/controller/BoardController.java
+++ GSupport/src/main/java/com/team_c/controller/BoardController.java
@@ -3,14 +3,10 @@ package com.team_c.controller;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.team_c.service.BoardService;
@@ -20,22 +16,15 @@ import com.team_c.service.BoardService;
 public class BoardController {
 	Logger log = Logger.getLogger(this.getClass());
 	
-	@Resource(name="boardService")
+	@Autowired
 	private BoardService boardService;
 	
-	//bossBoard매핑
-	@RequestMapping(value="/bossBoard", method = RequestMethod.GET)
-	public ModelAndView board(HttpServletRequest request) throws Exception {
-		ModelAndView mv = new ModelAndView("bossBoard");
+	@GetMapping("/board")
+	public ModelAndView board() {
+		ModelAndView mv = new ModelAndView();
 		List<Map<String, Object>> list = boardService.boardList();
 		mv.addObject("list", list);
-		
 		return mv;
-	}
-	
-	@GetMapping("/board.do")
-	public String board() {
-		return "board";
 	}
 	
 	
--- GSupport/src/main/java/com/team_c/service/BoardService.java
+++ GSupport/src/main/java/com/team_c/service/BoardService.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Service;
 @Service
 public interface BoardService {
 	List<Map<String, Object>> boardList();
-
 }
